afea9d65e34a8e5afa183061fc33c82ee2fe7ea2
Alemakave <savercat4@gmail.com>
2023-11-13 23:21:28 +1000

    * Cleanup code

--- src/main/java/ru/alemakave/mfstock/model/json/DateTimeJson.java
+++ src/main/java/ru/alemakave/mfstock/model/json/DateTimeJson.java
@@ -2,7 +2,6 @@ package ru.alemakave.mfstock.model.json;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.nio.file.attribute.FileTime;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -10,20 +9,12 @@ public class DateTimeJson {
     @JsonProperty("dateTime")
     private String dateTimeString;
 
-    public DateTimeJson(FileTime time, DateFormat dateFormat) {
-        this(Date.from(time.toInstant()), dateFormat);
-    }
-
     public DateTimeJson(Date date, DateFormat dateFormat) {
         this.dateTimeString = dateFormat.format(date);
     }
 
     public String getDateTimeString() {
         return dateTimeString;
-    }
-
-    public void setDateTimeString(FileTime time, DateFormat dateFormat) {
-        this.dateTimeString = dateFormat.format(time);
     }
 
     @Override
